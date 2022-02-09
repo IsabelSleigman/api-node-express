3ea96539604bdea05e7e9d24475ea6e0c7a75a30
IsabelSleigman <sleimusic@gmail.com>
2022-02-09 17:00:26 -0300

    finalizado requisições usuarios

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -23,6 +23,36 @@ const userRoute = (app) => {
 
             res.send({ users })
         })
+        .post((req,res) =>{
+            const users = getUsers()
+
+            users.push(req.body)
+            saveUser(users)
+
+            res.status(201).send('OK')
+        })
+        .put((req, res) => {
+            const users = getUsers()
+
+            saveUser(users.map(user => {
+                if(user.id == req.params.id){
+                    return{
+                        ...user,
+                        ...req.body
+                    }
+                }
+                return user
+            }))
+
+            res.status(200).send('Ok')
+        })
+        .delete((req, res) => {
+            const users = getUsers()
+
+            saveUser(users.filter(user => user.id != req.params.id))
+
+            res.status(200).send('Ok')
+        })
 }
 
 module.exports = userRoute
